a842c651432f614d71f695780b8d8e75cc054814
galdino <fgaldinoo@gmail.com>
2020-06-26 17:36:12 -0300

    Adding schema and application.properties

--- src/main/java/br/com/bookstore/repository/BookRepository.java
+++ src/main/java/br/com/bookstore/repository/BookRepository.java
@@ -2,9 +2,11 @@ package br.com.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import br.com.bookstore.model.BookEntity;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long>, JpaSpecificationExecutor<BookEntity> {
 
 }
--- src/main/java/br/com/bookstore/web/BookController.java
+++ src/main/java/br/com/bookstore/web/BookController.java
@@ -1,11 +1,17 @@
 package br.com.bookstore.web;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.bookstore.model.BookEntity;
 import br.com.bookstore.repository.BookRepository;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -17,8 +23,17 @@ public class BookController {
 	private BookRepository repository;
 	
 	@GetMapping("/hello")
-	public String getApp(){
+	public String getApp() {
 		return "Spring Boot Application";
+	}
+	
+	@GetMapping("/")
+	public List<BookEntity> getAllBooks() {
+		return this.repository.findAll();
+	}
+	
+	public BookEntity createBook(@Valid @RequestBody BookEntity book) {
+		return this.repository.save(book);
 	}
 
 }
