4c4cbd76c83cb52d8a36697f072104b828a3fd1c
galdino <fgaldinoo@gmail.com>
2020-06-29 17:05:12 -0300

    Adding Dockerfile and docker-compose.yml

--- src/test/java/br/com/bookstore/BookstoreApplicationTests.java
+++ src/test/java/br/com/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,13 @@
 package br.com.bookstore;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookstoreApplicationTests {
+public class BookstoreApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
