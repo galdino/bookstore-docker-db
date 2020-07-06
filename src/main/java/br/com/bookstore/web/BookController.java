b2e76f12e058833d0b9b862f8cd03fa0ea690ebe
galdino <fgaldinoo@gmail.com>
2020-07-06 14:57:00 -0300

    Adding ResponseEntity<T>

--- src/main/java/br/com/bookstore/web/BookController.java
+++ src/main/java/br/com/bookstore/web/BookController.java
@@ -1,12 +1,17 @@
 package br.com.bookstore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,17 +28,33 @@ public class BookController {
 	private BookRepository repository;
 	
 	@GetMapping("/hello")
-	public String getApp() {
-		return "Spring Boot Application";
+	public ResponseEntity<String> getApp() {
+		return new ResponseEntity<String>("Spring Boot Application", HttpStatus.OK);
 	}
 	
 	@GetMapping("/")
-	public List<BookEntity> getAllBooks() {
-		return this.repository.findAll();
+	public ResponseEntity<List<BookEntity>> getAllBooks() {
+		List<BookEntity> bookEntityList = this.repository.findAll();
+		if(bookEntityList.isEmpty()){
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List<BookEntity>>(bookEntityList, HttpStatus.OK);
+		}
 	}
 	
-	public BookEntity createBook(@Valid @RequestBody BookEntity book) {
-		return this.repository.save(book);
+	@GetMapping("/{serialNumber}")
+	public ResponseEntity<BookEntity> getOneBook(@PathVariable() Long serialNumber) {
+		Optional<BookEntity> bookEntityO = this.repository.findById(serialNumber);
+		if(bookEntityO.isPresent()){
+			return new ResponseEntity<BookEntity>(bookEntityO.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<BookEntity> createBook(@Valid @RequestBody BookEntity book) {
+		return new ResponseEntity<BookEntity>(this.repository.save(book), HttpStatus.CREATED);
 	}
 
 }
