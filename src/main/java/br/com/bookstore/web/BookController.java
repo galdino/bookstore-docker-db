f4a22bd8e45243caee1730bc3ea10076b3aef283
galdino <fgaldinoo@gmail.com>
2020-07-07 14:49:49 -0300

    Adding DELETE and PUT Mapping

--- src/main/java/br/com/bookstore/web/BookController.java
+++ src/main/java/br/com/bookstore/web/BookController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +34,7 @@ public class BookController {
 		return new ResponseEntity<String>("Spring Boot Application", HttpStatus.OK);
 	}
 	
-	@GetMapping("/")
+	@GetMapping
 	public ResponseEntity<List<BookEntity>> getAllBooks() {
 		List<BookEntity> bookEntityList = this.repository.findAll();
 		if(bookEntityList.isEmpty()){
@@ -43,7 +45,7 @@ public class BookController {
 	}
 	
 	@GetMapping("/{serialNumber}")
-	public ResponseEntity<BookEntity> getOneBook(@PathVariable() Long serialNumber) {
+	public ResponseEntity<BookEntity> getOneBook(@PathVariable(value="serialNumber") long serialNumber) {
 		Optional<BookEntity> bookEntityO = this.repository.findById(serialNumber);
 		if(bookEntityO.isPresent()){
 			return new ResponseEntity<BookEntity>(bookEntityO.get(), HttpStatus.OK);
@@ -52,9 +54,33 @@ public class BookController {
 		}
 	}
 	
-	@PostMapping("/")
+	@PostMapping
 	public ResponseEntity<BookEntity> createBook(@Valid @RequestBody BookEntity book) {
 		return new ResponseEntity<BookEntity>(this.repository.save(book), HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/{serialNumber}")
+	public ResponseEntity<BookEntity> deleteBook(@PathVariable(value="serialNumber") long serialNumber){
+		ResponseEntity<BookEntity> responseEntityAux = this.getOneBook(serialNumber);
+		if(responseEntityAux.getStatusCode().equals(HttpStatus.OK)){
+			this.repository.deleteById(serialNumber);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	@PutMapping("/{serialNumber}")
+	public ResponseEntity<BookEntity> updateBook(@PathVariable(value="serialNumber") long serialNumber,
+												 @Valid @RequestBody BookEntity book){
+		ResponseEntity<BookEntity> responseEntityAux = this.getOneBook(serialNumber);
+		if(responseEntityAux.getStatusCode().equals(HttpStatus.OK)){
+			book.setSerialNumber(serialNumber);
+			return new ResponseEntity<BookEntity>(this.repository.save(book), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
